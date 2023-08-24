e1d782e2274880f11e0335feae919e84d28a72e6
¨mateusdiias¨ <mateusdb30@gmail.com>
2023-08-24 17:40:28 -0300

    update implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -18,8 +18,6 @@ public class SellerDaoJDBC implements SellerDao {
 
     private Connection conn;
 
-    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
     public SellerDaoJDBC(Connection conn) {
         this.conn = conn;
     }
@@ -56,12 +54,29 @@ public class SellerDaoJDBC implements SellerDao {
         } finally {
             DB.closeStatement(st);
         }
-
     }
 
     @Override
     public void update(Seller obj) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("UPDATE seller\n" +
+                    "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ?\n" +
+                    "WHERE Id = ?");
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+            st.setInt(6, obj.getId());
 
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
