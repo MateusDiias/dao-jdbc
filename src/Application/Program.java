799f47db1dbb8604064add8b00b01a08b255d559
¨mateusdiias¨ <mateusdb30@gmail.com>
2023-08-24 17:05:40 -0300

    insert implementation

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -30,5 +31,10 @@ public class Program {
                 list) {
             System.out.println(obj);
         }
+
+        System.out.println("\nSeller insert");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("New id: " + newSeller.getId());
     }
 }
